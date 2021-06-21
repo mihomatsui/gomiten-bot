ad8ac4d30b18b7c686a95a3ff65ebf1cdf09401e
mihomatsui <mihomatsui.24@gmail.com>
2021-06-21 22:00:53 +0900

    line-bot-apiのコードを追加

--- app.rb
+++ app.rb
@@ -2,6 +2,35 @@ require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
 Dotenv.load
+
+def client
+  @client ||= Line::Bot::Client.new {
+    config.channel_id = ENV["LINE_CHANNEL_ID"]
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+  }
+end
+
+post '/callback' do
+  body = request.body.read
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+  unless client.validate_signature(body, signature)
+    error 400 do 'Bad Request' end
+  end
+  events = client.parse_events_from(body)
+  events.each do |event|
+    if event.is_a?(Line::Bot::Event::Message)
+      if event.type === Line::Bot::Event::MessageType::Text
+        message = {
+          type: 'text',
+          text: event.message['text']
+        }
+        client.reply_message(event['replyToken'], message)
+      end
+    end    
+  end
+  "OK"
+end
 get '/' do
   "Hello world!"
 end
