16b2aeb9448f0b388708916596f86a02d4ff9762
mihomatsui <mihomatsui.24@gmail.com>
2021-06-22 20:22:40 +0900

    clientメソッドを修正

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'sinatra/reloader' if development?
 Dotenv.load
 
 def client
-  @client ||= Line::Bot::Client.new {
+  @client ||= Line::Bot::Client.new { |config|
     config.channel_id = ENV["LINE_CHANNEL_ID"]
     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
