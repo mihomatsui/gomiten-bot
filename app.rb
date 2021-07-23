89ba99a64c38b7c1b996368a2cb1b0f26bf1226f
mihomatsui <mihomatsui.24@gmail.com>
2021-07-23 15:58:28 +0900

    webhookエラーの例外処理を追記

--- app.rb
+++ app.rb
@@ -14,11 +14,17 @@ end
 post '/callback' do
   body = request.body.read
   signature = request.env['HTTP_X_LINE_SIGNATURE']
+  end
   unless client.validate_signature(body, signature)
     error 400 do 'Bad Request' end
   end
   events = client.parse_events_from(body)
   events.each do |event|
+    if event.reply_token =="00000000000000000000000000000000": 
+    return
+    if event.reply_token == "ffffffffffffffffffffffffffffffff":
+    return
+
     if event.is_a?(Line::Bot::Event::Message)
       if event.type === Line::Bot::Event::MessageType::Text
         message = {
