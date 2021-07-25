b616fcbbcd3aa586d676beaa7580335a22b37da2
mihomatsui <mihomatsui.24@gmail.com>
2021-07-25 16:21:31 +0900

    例外処理を外す

--- app.rb
+++ app.rb
@@ -24,9 +24,6 @@ post '/callback' do
           type: 'text',
           text: event.message['text']
         }
-        if event['replyToken']="00000000000000000000000000000000"
-          return 
-        end
         client.reply_message(event['replyToken'], message)
       end
     end    
