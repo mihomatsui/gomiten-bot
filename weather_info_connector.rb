dc1dc6a3610c830dd3153c1de79c7e59266ac788
mihomatsui <mihomatsui.24@gmail.com>
2021-08-17 23:57:32 +0900

    rexml/documentライブラリを導入

--- weather_info_connector.rb
+++ weather_info_connector.rb
@@ -1,1 +1,11 @@
+require 'net/http'
 require 'uri'
+require 'rexml/document'
+
+uri = URI.parse("http://www.drk7.jp/weather/xml/01.xml")
+response = Net::HTTP.get_response(uri)
+p response.code
+p response.body
+
+# サイトからxmlファイルを持ってくる
+# xmlファイルから必要な項目を取り出す
