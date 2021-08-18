42060c9a4cfba684d553b3f06c7dda498a651af6
mihomatsui <mihomatsui.24@gmail.com>
2021-08-18 23:59:57 +0900

    天気の取得の詳細を追加

--- weather_info_connector.rb
+++ weather_info_connector.rb
@@ -2,10 +2,25 @@ require 'net/http'
 require 'uri'
 require 'rexml/document'
 
-uri = URI.parse("http://www.drk7.jp/weather/xml/01.xml")
-response = Net::HTTP.get_response(uri)
-p response.code
-p response.body
+# 北海道上川地方の今日の天気を取得する場合
+# weather = WeatherInfoConnector.new('北海道', '上川地方', 'http://www.drk7.jp/weather/xml/01.xml', 'weatherforecast/pref/area[1]', 0)
 
-# サイトからxmlファイルを持ってくる
-# xmlファイルから必要な項目を取り出す
+# 天気情報の取得
+weather = WeatherInfoConnector.new(pref, area, url, xpath, set_day)
+puts weather.WeatherInfoConnector
+
+class WeatherInfoConnector
+  def get_weatherinfo
+    # URIをparse(解析)
+    uri = URI.parse(url)
+    # ウェブサーバからドキュメントを得る (GET)
+    xml = Net::HTTP.get(uri)
+    # XMLを指定したインスタンスを作成してDOMツリーを構築
+    doc = REXML::Document.new(xml)
+
+    # 
+    # 各情報を取得
+    
+  end
+  
+end
