dd8f88ed0f8a05e9200971930cc2ceae7371034f
mihomatsui <mihomatsui.24@gmail.com>
2021-08-19 00:14:09 +0900

    casewhen文を追加

--- weather_info_connector.rb
+++ weather_info_connector.rb
@@ -17,10 +17,18 @@ class WeatherInfoConnector
     xml = Net::HTTP.get(uri)
     # XMLを指定したインスタンスを作成してDOMツリーを構築
     doc = REXML::Document.new(xml)
-
-    # 
+ 
     # 各情報を取得
-    
+    case set_day
+    when 0
+      fix_xpath = xpath + '/info[1]' #今日
+    when 1
+      fix_xpath = xpath + '/info[2]' #明日
+    when 2
+      fix_xpath = xpath + "/info[3]" #明後日
+    else
+      fix_xpath = xpath + "/info[0]" #今日
+    end
   end
   
 end
