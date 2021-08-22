2eb6219793929c9ad5175fb2a037b61a907883ac
mihomatsui <mihomatsui.24@gmail.com>
2021-08-22 23:51:35 +0900

    SQL文を追加

--- weather_db_connector.rb
+++ weather_db_connector.rb
@@ -17,9 +17,22 @@ class WeatherDbConnector
 
   def set_location(user_id, latitude, longitude)
     p 'set_location'
+    # 絶対値格納用のカラムを追加
+    # addsql = 'ALTER TABLE weathers ADD COLUMN abs decimal;'
+    # ActiveRecord::Base.connection.execute(addsql)
+
     # 絶対値を取得し一番小さい値の地域を設定する
-    calculate_point = %{(latitude - #{latitude}) + (longitude - #{longitude})}
-    result = Weather.select("weathers, ABS(point) AS calculate_point").order(:calculate_point :asc).first
+    calculatepoint = '(latitude - #{latitude}) + (longitude - #{longitude})'
+    calculatepoint_abs = abs(calculatepoint)
+    insertsql = 'insert into weathers (abs) values (calculatepoint_abs);'
+    searchsql = 'select * from weathers order by abs[asc].first'
+    result = ActiveRecord::Base.connection.select_all(searchsql).to_hash
+    
     puts %{#{result['id']},#{result['pref']},#{result['area']},#{result['latitude']}, #{result['longitude']}}
+    return result["pref"], result["area"]
+
+    # 絶対値格納用のカラムを削除
+    # deletesql = 'ALTER TABLE weathers DROP COLUMN abs numeric;'
+    # ActiveRecord::Base.connection.execute(deletesql)
   end
 end
