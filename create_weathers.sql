a606da968d89fb04a68a52b7030600e59a9c0810
mihomatsui <mihomatsui.24@gmail.com>
2021-08-26 11:08:52 +0900

    numericに変更

--- create_weathers.sql
+++ create_weathers.sql
@@ -2,8 +2,8 @@ create table weathers (
   id int primary key, 
   pref varchar(32),
   area varchar(32),
-  latitude decimal,
-  longitude decimal,
+  latitude numeric,
+  longitude numeric,
   url varchar(64),
   xpath varchar(64)
 )
