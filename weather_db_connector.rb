46db6a7f517721c5addd583c2b8d4637ca8492ff
mihomatsui <mihomatsui.24@gmail.com>
2021-09-08 18:45:51 +0900

    activerecordの記述を削除

--- weather_db_connector.rb
+++ weather_db_connector.rb
@@ -1,5 +1,5 @@
-require 'active_record'
 require 'dotenv/load'
+require 'pg'
 
 class WeatherDbConnector
   DEFAULT_WEATHER_HOUR = 7
@@ -7,19 +7,16 @@ class WeatherDbConnector
   DEFAULT_AREA_ID = 1
 
   # 環境変数を使って接続する
-  ActiveRecord::Base.establish_connection(
-    adapter: ENV.fetch("myadapter"),
-    host: "",
-    username: ENV.fetch("myusername"),
-    password: ENV.fetch("mypassword"),
-    database: ENV.fetch("mydatabase")
-  )
-
-  class Weather < ActiveRecord::Base
+  def connect
+    @@client = PG::Connection.new(
+      :host => "",
+      :user => ENV.fetch("DB_USER"),
+      :password => ENV.fetch("DB_PASSWORD"),
+      :database => ENV.fetch("DB_NAME"),
+    )
   end
-
+  
   def initialize
-    @conn = ActiveRecord::Base.connection
     # 毎回リセットする
     drop_weathers
     # テーブルの作成
@@ -32,13 +29,13 @@ class WeatherDbConnector
     p 'create_weathers_table'
     File.open('create_weathers.sql', 'r:utf-8') do |f|
       createsql = f.read
-      @conn.execute(createsql)
+      @@client.execute(createsql)
     end
 
     p 'create_notifications_table'
     File.open('notification.sql', 'r:utf-8') do |f|
       notificationsql = f.read
-      @conn.execute(notificationsql)
+      @@client.execute(notificationsql)
     end
   end
 
@@ -46,43 +43,43 @@ class WeatherDbConnector
     p 'insert_weathers_table'
     File.open('insert_weathers.sql', 'r:utf-8') do |f|
       f.each_line do |createsql|
-        @conn.execute(createsql)
+        @@client.execute(createsql)
       end
     end
   end
 
   def drop_weathers
     p 'drop_weathers_table'
-    @conn.execute("drop table if exists weathers")
-    @conn.execute("drop table if exists notifications")
+    @@client.execute("drop table if exists weathers")
+    @@client.execute("drop table if exists notifications")
   end
 
   def notification_enable_user(user_id)
     p 'enable_user'
-    @conn.execute("insert into notifications (user_id, hour, minute, area_id, notificationDisabled) values ('#{user_id}', #{DEFAULT_WEATHER_HOUR},#{DEFAULT_WEATHER_MINUTE}, #{DEFAULT_AREA_ID}, false) on conflict on constraint notifications_pkey do update set user_id = excluded.user_id, notificationDisabled = excluded.notificationDisabled;")
+    @@client.execute("insert into notifications (user_id, hour, minute, area_id, notificationDisabled) values ('#{user_id}', #{DEFAULT_WEATHER_HOUR},#{DEFAULT_WEATHER_MINUTE}, #{DEFAULT_AREA_ID}, false) on conflict on constraint notifications_pkey do update set user_id = excluded.user_id, notificationDisabled = excluded.notificationDisabled;")
   end
 
   def notification_disnable_user(user_id)
     p 'disnable_user'
-    @conn.execute("insert into notifications (user_id, hour,minute, area_id, notificationDisabled) values ('#{user_id}', #{DEFAULT_WEATHER_HOUR},#{DEFAULT_WEATHER_MINUTE}, #{DEFAULT_AREA_ID}, true) on conflict on constraint notifications_pkey do update set user_id = excluded.user_id, notificationDisabled = excluded.notificationDisabled;")
+    @@client.execute("insert into notifications (user_id, hour,minute, area_id, notificationDisabled) values ('#{user_id}', #{DEFAULT_WEATHER_HOUR},#{DEFAULT_WEATHER_MINUTE}, #{DEFAULT_AREA_ID}, true) on conflict on constraint notifications_pkey do update set user_id = excluded.user_id, notificationDisabled = excluded.notificationDisabled;")
   end
 
   def set_time(user_id, hour, minute)
     p 'set_time'
-    @conn.execute("insert into notifications (user_id, hour,minute, area_id, notificationDisabled) values ('#{user_id}', #{hour},#{minute}, #{DEFAULT_AREA_ID}, true) on conflict on constraint notifications_pkey do update set user_id = excluded.user_id, hour = excluded.hour, minute = excluded.minute;")
+    @@client.execute("insert into notifications (user_id, hour,minute, area_id, notificationDisabled) values ('#{user_id}', #{hour},#{minute}, #{DEFAULT_AREA_ID}, true) on conflict on constraint notifications_pkey do update set user_id = excluded.user_id, hour = excluded.hour, minute = excluded.minute;")
   end
 
   def set_location(user_id, latitude, longitude)
     p 'set_location'
-    result = @conn.execute("select * from weathers order by abs(latitude - #{latitude}) + abs(longitude - #{longitude}) asc;").first
+    result = @@client.execute("select * from weathers order by abs(latitude - #{latitude}) + abs(longitude - #{longitude}) asc;").first
     puts "#{result["id"]},#{result["pref"]},#{result["area"]},#{result["latitude"]},#{result["longitude"]}"
-    @conn.execute("insert into notifications (user_id, hour, minute, area_id) values ('#{user_id}', #{DEFAULT_WEATHER_HOUR},#{DEFAULT_WEATHER_MINUTE},'#{result["id"]}') on conflict on constraint notifications_pkey do update set user_id = excluded.user_id, area_id = excluded.area_id;")
+    @@client.execute("insert into notifications (user_id, hour, minute, area_id) values ('#{user_id}', #{DEFAULT_WEATHER_HOUR},#{DEFAULT_WEATHER_MINUTE},'#{result["id"]}') on conflict on constraint notifications_pkey do update set user_id = excluded.user_id, area_id = excluded.area_id;")
     return result["pref"], result["area"]
   end
 
   def get_all_notifications
    p 'get_all_notifications'
-   results = @conn.execute('select * from notifications inner join weathers on notifications.area_id = weathers.id;')
+   results = @@client.execute('select * from notifications inner join weathers on notifications.area_id = weathers.id;')
    results.each do |row|
     puts "----------------------------"
     p row
@@ -92,7 +89,7 @@ class WeatherDbConnector
 
   def get_notifications(user_id)
     p 'get_notifications(user_id)'
-    results = @conn.execute("select * from notifications inner join weathers on notifications.area_id = weathers.id where notifications.user_id = '#{user_id}';")
+    results = @@client.execute("select * from notifications inner join weathers on notifications.area_id = weathers.id where notifications.user_id = '#{user_id}';")
     results.each do |row|
       puts "----------------------------"
       p row
@@ -102,6 +99,6 @@ class WeatherDbConnector
 
   def fix_notifications
     p 'fix_notifications'
-    @conn.execute("update notifications set hour = 7, minute = 0 where hour is null")
+    @@client.execute("update notifications set hour = 7, minute = 0 where hour is null")
   end
 end
