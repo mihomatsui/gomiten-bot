6bff8dab3466d89b7170afc02414ea1819f25f96
mihomatsui <mihomatsui.24@gmail.com>
2021-09-19 11:06:25 +0900

    エンコードの推測を追加

--- preparation_address.rb
+++ preparation_address.rb
@@ -4,3 +4,7 @@ require 'csv'
 #CSVファイルのパスを指定
 path = 'path/to/x-ken-all.csv'
 
+# ファイル読み込みとエンコードを推測
+detection = CharlockHolmes::EncodingDetector.detect(File.read(path))
+# CP932 を優先する
+encoding = detection[:encoding] == 'Shift_JIS' ? 'CP932' : detection[:encoding]
