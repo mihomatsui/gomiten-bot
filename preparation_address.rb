3153848956d0347ef8b4d51e120135ea18271287
mihomatsui <mihomatsui.24@gmail.com>
2021-09-19 20:16:36 +0900

    csvファイルの読み込みを確認

--- preparation_address.rb
+++ preparation_address.rb
@@ -1,18 +1,5 @@
-#require 'charlock_holmes'
 require 'csv'
-
-#CSVファイルのパスを指定
-#path = 'path/to/x-ken-all.csv'
-
-# ファイル読み込みとエンコードを推測
-#detection = CharlockHolmes::EncodingDetector.detect(File.read(path))
-
-# CP932 を優先する
-#encoding = detection[:encoding] == 'Shift_JIS' ? 'CP932' : detection[:encoding]
-
-# CSVを1行ずつUTF-8として読み込む
-# CSV.foreach(path,
-#             encoding: "#{encoding}:UTF-8",
-#             headers: true) do |row|
-#   p row.inspect
-# end
+CSV.foreach("x-ken-all.csv") do |row|
+  # 行に対する処理
+  p row
+end
