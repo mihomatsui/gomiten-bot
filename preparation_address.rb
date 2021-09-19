5b7ed916a3ac7017ee69270bbc453fec13e436d5
mihomatsui <mihomatsui.24@gmail.com>
2021-09-19 23:37:30 +0900

    csvにヘッダを追加

--- preparation_address.rb
+++ preparation_address.rb
@@ -1,5 +1,18 @@
 require 'csv'
-CSV.foreach("x-ken-all.csv") do |row|
-  # 行に対する処理
-  p row
+header = ["government_code", "old_postalcode", "postalcode", "kana_pref", "kana_municipalities", "kana_townarea", "pref", "municipalities", "townarea", "multiple_postalcode", "koaza_townarea", "city_block", "multiple_townarea", "update", "update_reason"]
+
+data = CSV.foreach("x-ken-all.csv")
+
+# CSVデータを生成する
+CSV.open('head-x-ken-all.csv','w') do |csv|
+  csv << header #ヘッダ行をCSVに追加
+  data.each do |d|
+    csv << d #データ行をCSVに追加
+  end
+end
+
+# 生成したCSVデータを読み込む
+# CSV.foreachの引数にheaders: trueとつけて先頭行をヘッダとして認識する
+CSV.foreach("head-x-ken-all.csv", headers: true) do |row|
+  p row.to_h
 end
