b40b26073149bde8625187ce980940feaa235222
mihomatsui <mihomatsui.24@gmail.com>
2021-09-19 15:44:10 +0900

    コメントアウト

--- preparation_address.rb
+++ preparation_address.rb
@@ -1,17 +1,18 @@
-require 'charlock_holmes'
+#require 'charlock_holmes'
 require 'csv'
 
 #CSVファイルのパスを指定
-path = 'path/to/x-ken-all.csv'
+#path = 'path/to/x-ken-all.csv'
 
 # ファイル読み込みとエンコードを推測
-detection = CharlockHolmes::EncodingDetector.detect(File.read(path))
+#detection = CharlockHolmes::EncodingDetector.detect(File.read(path))
+
 # CP932 を優先する
-encoding = detection[:encoding] == 'Shift_JIS' ? 'CP932' : detection[:encoding]
+#encoding = detection[:encoding] == 'Shift_JIS' ? 'CP932' : detection[:encoding]
 
 # CSVを1行ずつUTF-8として読み込む
-CSV.foreach(path,
-            encoding: "#{encoding}:UTF-8",
-            headers: true) do |row|
-  p row.inspect
-end
+# CSV.foreach(path,
+#             encoding: "#{encoding}:UTF-8",
+#             headers: true) do |row|
+#   p row.inspect
+# end
