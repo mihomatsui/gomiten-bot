86910287a253bdeffb74b87e9cba989a9b117fff
mihomatsui <mihomatsui.24@gmail.com>
2021-09-22 08:19:07 +0900

    addressの項目を追加

--- app.rb
+++ app.rb
@@ -88,6 +88,7 @@ post '/callback' do
         # 位置情報を取得
         latitude = event.message['latitude']
         longitude = event.message['longitude']
+        address = event.message['address']
         puts "位置情報を取得しました！"
         pref, area = $db.set_location(user_id, latitude, longitude)
         reply_text = %{地域を#{pref} #{area}にセットしました！\n\n「3」または「天気」と入力すると、現在設定されている地域の天気をお知らせします。}
