3f59c2bd4e5589dabce30660bdac9c2418712d64
mihomatsui <mihomatsui.24@gmail.com>
2021-09-25 00:24:15 +0900

    データ挿入のファイルを作成

--- preparation_sql.rb
+++ preparation_sql.rb
@@ -1,17 +1,18 @@
 require "csv"
 # insert文を生成
-reader = CSV.open("index-select-ken-all.csv", "r")
+reader = CSV.open("insert_id_address.csv", "r")
 reader.shift
 
-sql = "insert into addresses (id, postalcode, pref, municipalities, townarea) values \n"
+sql = "insert into addresses (id, pref, municipalities, townblock, latitude, longitude) values \n"
 reader.each do |line|
   id = line[0].to_s
-  postalcode = line[1].to_s
-  pref = line[2].to_s
-  municipalities = line[3].to_s
-  townarea = line[4].to_s
+  pref = line[1].to_s
+  municipalities = line[2].to_s
+  townblock = line[3].to_s
+  latitude = line[4].to_s
+  longitude = line[5].to_s
   insertValue =
-    "(" + id + ", " + postalcode + ",'" + pref + "','" + municipalities + "','" + townarea + "'),\n"
+    "(" + id + ", '" + pref + "','" + municipalities + "','" + townblock + "'," + latitude + "," + longitude + "),\n"
   sql += insertValue
 end
 
