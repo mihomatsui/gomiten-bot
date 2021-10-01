7b3db70427744e8584ed5e7689725e909f6f699b
mihomatsui <mihomatsui.24@gmail.com>
2021-10-01 20:49:18 +0900

    第何回目かと曜日を定義

--- date_check.rb
+++ date_check.rb
@@ -1,10 +1,15 @@
-class Date
-  # 第n週目
-  def nweek
-    (self.day + 6 + (self - self.day + 1).wday) / 7
-  end
+class DateChecker
+  def self.nth_day_of_week(now: Time.now)
+    # 曜日を定義
+    wdays = { 0 => "日", 1 => "月", 2 => "火", 3 => "水", 4 => "木", 5 => "金", 6 =>"土"}
 
-  def self.tomorrow
-    self.today + 1
+    # 第何回目かを定義
+    nth = (now.day + 1) / 8 + 1
+
+    # 第何回目かと翌日の曜日を返す
+    return {nth: nth, wday: wdays[now.wday + 1]}
   end
 end
+
+datechecker = DateChecker.new
+puts DateChecker.nth_day_of_week
