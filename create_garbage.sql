76f56c98c110f682d89f21e8d1f36e8c2ad6629e
mihomatsui <mihomatsui.24@gmail.com>
2021-10-23 14:05:05 +0900

    enum型に変更

--- create_garbage.sql
+++ create_garbage.sql
@@ -1,8 +1,11 @@
+create type week as enum('1', '2', '3', '4', '毎週');
+create type wday as enum('日', '月', '火', '水', '木', '金', '土');
+
 create table garbage (
   id int primary key, 
   address_id int references addresses(id),
-  week int,
-  wday int,
+  weektype week,
+  wdaytype wday
   category_id int references category(id)
 )
 
