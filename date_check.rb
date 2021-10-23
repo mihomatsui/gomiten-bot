87ff37109f6e4b59397507409deece2ce9330fb6
mihomatsui <mihomatsui.24@gmail.com>
2021-10-23 14:05:30 +0900

    n週目の書き方を変更

--- date_check.rb
+++ date_check.rb
@@ -1,15 +1,11 @@
-class DateChecker
-  def self.nth_day_of_week(now: Time.now)
-    # 曜日を定義
-    wdays = { 0 => "日", 1 => "月", 2 => "火", 3 => "水", 4 => "木", 5 => "金", 6 =>"土"}
+class Date
+  # 第n週目
+  def nweek
+    now.day / 8 + 1
+  end
 
-    # 第何回目かを定義
-    nth = (now.day + 1) / 8 + 1
-
-    # 第何回目かと翌日の曜日を返す
-    return {nth: nth, wday: wdays[now.wday + 1]}
+  def self.tomorrow
+    self.today + 1
   end
 end
 
-datechecker = DateChecker.new
-puts DateChecker.nth_day_of_week
