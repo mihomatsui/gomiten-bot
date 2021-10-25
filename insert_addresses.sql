14038b72568f98ad9a825bed5f29cc002eeb8b4e
mihomatsui <mihomatsui.24@gmail.com>
2021-10-25 21:18:58 +0900

    UPSERTの処理を追加

--- insert_addresses.sql
+++ insert_addresses.sql
@@ -1,4 +1,5 @@
 insert into addresses (id, pref, municipalities, townblock) values 
 (0, '愛知県', '名古屋市', '西区浅間一丁目'),
-(1, '愛知県', '名古屋市', '西区浅間二丁目');
+(1, '愛知県', '名古屋市', '西区浅間二丁目')on conflict (id) do update set pref = excluded.pref, municipalities = excluded.municipalities, townblock = excluded.townblock;
+
 
