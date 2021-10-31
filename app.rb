4704043ccd97a60696a4ed7e26735d9a394a2366
mihomatsui <mihomatsui.24@gmail.com>
2021-10-31 19:28:24 +0900

    項目名変更に伴うupsert変更

--- app.rb
+++ app.rb
@@ -40,19 +40,15 @@ def client
 end
 
 get '/send' do
-  protected! #basic認証
+  #protected! #basic認証
   weather_info_conn = WeatherInfoConnector.new
   #now_time = Time.now
   begin
   $db.get_all_notifications.each do |row|
-    if row['notificationDisabled'] == false then
-      #hour = row['hour'] || 7
-      #minute = row['minute'] || 0
-      #next if hour != (now_time.hour + 9) % 24 # GMTからJISに変換 早期リターン
-      #next if minute != now_time.min
-      set_day = hour < 6 ? 1 : 0 # weatherapiは朝6時に更新
+      p 'おはよう'
+      set_day = 0
       forecast = weather_info_conn.get_weatherinfo(row['pref'], row['area'], row['url'].sub(/http/, 'https'), row['xpath'], set_day)
-      puts %{7:00 #{forecast}}
+      puts forecast
       message = { type: 'text', text: forecast }
       p 'push message'
 
@@ -60,12 +56,32 @@ get '/send' do
       when /.*(雨|雪).*/ 
         message_sticker = {"type": "sticker", "packageId": "446", "stickerId": "1994"}
         messages = [message, message_sticker]
+        
         p client.push_message(row['user_id'], messages)
       else
         p client.push_message(row['user_id'], message)
       end
     end
-  end
+    # if row['notificationDisabled'] == false then
+    #   #hour = row['hour'] || 7
+    #   #minute = row['minute'] || 0
+    #   #next if hour != (now_time.hour + 9) % 24 # GMTからJISに変換 早期リターン
+    #   #next if minute != now_time.min
+    #   set_day = hour < 6 ? 1 : 0 # weatherapiは朝6時に更新
+    #   forecast = weather_info_conn.get_weatherinfo(row['pref'], row['area'], row['url'].sub(/http/, 'https'), row['xpath'], set_day)
+    #   puts %{7:00 #{forecast}}
+    #   message = { type: 'text', text: forecast }
+    #   p 'push message'
+
+    #   case forecast
+    #   when /.*(雨|雪).*/ 
+    #     message_sticker = {"type": "sticker", "packageId": "446", "stickerId": "1994"}
+    #     messages = [message, message_sticker]
+    #     p client.push_message(row['user_id'], messages)
+    #   else
+    #     p client.push_message(row['user_id'], message)
+    #   end
+    # end
   rescue => e
     p e
   end
