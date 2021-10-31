50729ae724cfd047935cc46f758e992bd9dd0e47
mihomatsui <mihomatsui.24@gmail.com>
2021-11-01 00:43:34 +0900

    実際のデータに更新

--- sen2_date_check.rb
+++ sen2_date_check.rb
@@ -25,30 +25,24 @@ class GarbageDate
   def nth_day_of_week(date)
     (date.beginning_of_month..date).count { _1.wday == date.wday }
   end
-end  
 
-GARBAGE_DISPOSAL_DAYS = [
+  GARBAGE_DISPOSAL_DAYS = [
   # 毎週火・金は可燃ごみの日
-  *%w(火 金).product([*1..5]).map { |wday, nth| GarbageDate.new(wday: wday, nth: nth, type: '可燃ゴミ')},
-  GarbageDate.new(wday: '水', nth: 4, type: '不燃ゴミ'),
-  GarbageDate.new(wday: '木', nth: 4, type: 'プラスチックゴミ'),
-  GarbageDate.new(wday: '木', nth: 4, type: '缶・ビン・ペットボトル')
-]
-tomorrow = Date.tomorrow
-garbage_disposal_day = GARBAGE_DISPOSAL_DAYS.find {_1.apply?(tomorrow)}
+    *%w(火 金).product([*1..5]).map { |wday, nth| GarbageDate.new(wday: wday, nth: nth, type: '可燃ゴミ')},
+    GarbageDate.new(wday: '水', nth: 4, type: '不燃ゴミ'),
+    *%w(木).product([*1..5]).map { |wday, nth| GarbageDate.new(wday: wday, nth: nth, type: 'プラスチックゴミ')},
+    *%w(水).product([*1..5]).map { |wday, nth| GarbageDate.new(wday: wday, nth: nth, type: '缶・ビン・ペットボトル')}
+  ]
+  tomorrow = Date.tomorrow
+  garbage_disposal_day = GARBAGE_DISPOSAL_DAYS.find {_1.apply?(tomorrow)}
 
-# def message_check
-#   message = case check
-#   when garbage_disposal_day then '#{garbage_disposal_day}の日です'
-#   else '特に出せるゴミはありません'
-#   end
-#    puts "明日(#{tomorrow.strftime("%m月%d日")})は#{message}"
-# end
-message = ''
-message << %{明日(#{tomorrow.strftime("%m月%d日")})は}
-if garbage_disposal_day
-  message << %{#{garbage_disposal_day}の日です}
-else 
-  message << %{特に出せるゴミはありません}
+  message = ''
+  message << %{明日(#{tomorrow.strftime("%m月%d日")})は}
+  if garbage_disposal_day
+    message << %{#{garbage_disposal_day}の日です}
+  else 
+    message << %{特に出せるゴミはありません}
+  end
+  message
 end
-puts message
+
