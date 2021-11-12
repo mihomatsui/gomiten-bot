ac88e10cd95f2c3cac9bda14f6931c6a48435c42
mihomatsui <mihomatsui.24@gmail.com>
2021-11-12 12:33:28 +0900

    FKを修正

--- sql/create_garbages.sql
+++ sql/create_garbages.sql
@@ -1,6 +1,6 @@
 create table if not exists garbages (
   id int primary key, 
-  nth varchar(32),
-  wday varchar(32),
+  foreign key (nth_id) references nth(id),
+  foreign key (wday_id) references wday(id),
   foreign key (category_id) references category(id)
 )
