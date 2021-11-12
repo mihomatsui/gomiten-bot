c09aedc46bfa508dee35e876a3d88bccbf88b5b5
mihomatsui <mihomatsui.24@gmail.com>
2021-11-12 12:47:59 +0900

    データを仮入れ

--- sql/create_garbages.sql
+++ sql/create_garbages.sql
@@ -1,5 +1,6 @@
 create table if not exists garbages (
   id int primary key, 
+  area varchar(32),
   foreign key (nth_id) references nth(id),
   foreign key (wday_id) references wday(id),
   foreign key (category_id) references category(id)
---
+++ sql/insert_garbages.sql
@@ -0,0 +1,5 @@
+insert into garbages (id, area, nth_id, wday_id, category_id) values (0, '名古屋市西区数奇屋', 0, 1, 0)on conflict do nothing;
+insert into garbages (id, area, nth_id, wday_id, category_id) values (1, '名古屋市西区数奇屋', 0, 5, 0)on conflict do nothing;
+insert into garbages (id, area, nth_id, wday_id, category_id) values (2, '名古屋市西区数奇屋', 4, 4, 1)on conflict do nothing;
+insert into garbages (id, area, nth_id, wday_id, category_id) values (3, '名古屋市西区数奇屋', 0, 6, 2)on conflict do nothing;
+insert into garbages (id, area, nth_id, wday_id, category_id) values (3, '名古屋市西区数奇屋', 0, 4, 3)on conflict do nothing;
